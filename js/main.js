3b1cafe376fc69243c4532562fc1335e936fc7a5
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-08-27 16:42:03 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator){
   window.addEventListener('load',async()=>{
    try{
-     const registration = await navigator.serviceWorker.register('./../sw_cached_site.js')
+     const registration = await navigator.serviceWorker.register('./../sw_cached_pages.js')
      console.log(`service worker registered`)
      console.log(registration)
 
